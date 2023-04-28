b72335d0ccfc7b987a99408d492518a6334e1571
technoprogramming92 <technoprogramming92@gmail.com>
2023-04-28 16:37:23 +0530

    Linking of services on home page done2

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import SectionTitle from "../Common/SectionTitle";
 import SingleBlog from "./SingleBlog";
 import blogData from "./blogData";
+
 
 const Blog = () => {
   return (
@@ -20,6 +22,15 @@ const Blog = () => {
           ))}
         </div>
       </div>
+      <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href="https://nextjstemplates.com/templates/startup"
+                    className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                  >
+                    Load More
+                  </Link>
+                  
+                </div>
     </section>
   );
 };
