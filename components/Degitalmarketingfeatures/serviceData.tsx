6e017b39978e9297594a3639bcbe8cd9e43272d3
Purvi Trivedi <ignitax@gmail.com>
2023-05-04 17:05:31 +0530

    degital media change by purvi

---
+++ components/Degitalmarketingfeatures/serviceData.tsx
@@ -0,0 +1,53 @@
+import { Service } from "@/types/service";
+
+
+const serviceData: Service[] = [
+  {
+    id: 1,
+    title: "Social media management:",
+    paragraph:
+      "In the digital age, social media is a crucial aspect of any business's marketing strategy. Our social media marketing services focus on creating a strong presence for your brand on all major social media platforms, including Facebook, Twitter, Instagram, LinkedIn, and more. We'll work with you to develop engaging content that resonates with your audience and creates meaningful interactions and With our data-driven approach, you'll be able to make informed decisions about your social media marketing strategy and see real results.",
+    image: "/images/service-home-real/Tax-bro.svg",
+  },
+  {
+    id: 2,
+    title: "Content creation",
+    paragraph:
+      "Our team of expert content creators can produce everything from eye-catching graphics and captivating videos to engaging blog posts and social media updates. We'll work closely with you to understand your brand's unique voice and develop content that aligns with your goals and resonates with your audience.Whether you need a one-time project or ongoing content creation support, we're here to help. With [Company Name], you can be confident that your content will be high-quality, on-brand, and designed to drive results for your business.",
+    image: "/images/service-home-real/Tax-amico.svg",
+  },
+  
+  {
+    id: 3,
+    title: "Email Marketing",
+    paragraph:
+      "With our targeted email marketing campaigns, you can stay at the forefront of your customer's minds and get your message across in an effective and personalized way. We'll help you craft emails that not only look great but also deliver results, whether it's driving traffic to your website or increasing conversions. With our expertise in email marketing, you can be confident that your emails will stand out in a crowded inbox and get noticed by the right people.",
+    image: "/images/service-home-real/Website Creator-amico.svg",
+  },
+  {
+    id: 4,
+    title: "Search Engine Optimization (SEO)",
+    paragraph:
+      "SEO Virtual services Provided by IGNITAX involve optimizing your website's content and structure to make it more visible and attractive to search engines. This means more potential clients will find you when searching for the services you offer. We use industry-standard techniques to ensure your website appears at the top of search engine results pages for relevant keywords and phrases. This can lead to more organic traffic and greater exposure for your business. ",
+    image: "/images/service-home-real/Code typing-amico.svg",
+  },
+  {
+    id: 5,
+    title: "Pay-per-click (PPC) advertising",
+    paragraph:
+      "We can help you get your brand in front of even more potential customers.We'll design and manage campaigns that are targeted to your ideal audience, Our team uses advanced analytics and data tracking tools to measure the effectiveness of your campaigns and make real-time adjustments, so you can maximize your return on investment (ROI). Whether you're looking to increase brand awareness, drive traffic to your website, or generate more leads and sales, our PPC services can help you reach your goals.",
+    image: "/images/service-home-real/Marketing consulting-pana.svg",
+  },
+  {
+    id: 6,
+    title: "Analytics and Reporting",
+    paragraph:
+      "Our team of experts will provide you with in-depth reports that give you a clear understanding of the performance of your website and marketing efforts. We'll help you identify which channels and campaigns are driving the most traffic, conversions, and revenue, so you can make informed decisions about where to allocate your marketing budget. With our help, you can optimize your marketing efforts and get the most out of your investment.",
+    image: "/images/service-home-real/Marketing consulting-pana.svg",
+  },
+  
+  
+  
+  
+];
+export default serviceData;
